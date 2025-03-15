e4e8579398624a3de126791157a7ddf1303b3254
Jackson Tian <shyvo1987@gmail.com>
2025-03-15 23:20:59 +0800

    init project

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: { 
+      globals: {
+        ...globals.node,
+        describe: "readonly", // 添加 describe 全局变量
+        it: "readonly"        // 添加 it 全局变量
+      } 
+    },
+    rules: {
+      "indent": ["error", 2],
+      "no-unused-vars": ["error", { args: "none" }] // 修改规则以忽略未使用的局部变量
+    }
+  },
+  pluginJs.configs.recommended,
+];
