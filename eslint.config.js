447196a935510492c86cf2edba8c2d45d6f07ef3
Jackson Tian <shyvo1987@gmail.com>
2025-04-01 11:38:48 +0800

    improve router & add middleware

--- eslint.config.js
+++ eslint.config.js
@@ -4,15 +4,16 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    languageOptions: { 
+    languageOptions: {
       globals: {
         ...globals.node,
         describe: "readonly", // 添加 describe 全局变量
         it: "readonly"        // 添加 it 全局变量
-      } 
+      }
     },
     rules: {
       "indent": ["error", 2],
+      "no-trailing-spaces": "error",
       "no-unused-vars": ["error", { args: "none" }] // 修改规则以忽略未使用的局部变量
     }
   },
