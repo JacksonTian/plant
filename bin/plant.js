213d77893f2d5c495ce51e1515e22bc13cdbb1c2
Jackson Tian <shyvo1987@gmail.com>
2025-04-24 11:43:16 +0800

    Add close hook for application

--- bin/plant.js
+++ bin/plant.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
 import path from 'path';
-
 import minimist from 'minimist';
-
 import plant from '../lib/plant.js';
 
 const argv = minimist(process.argv.slice(2));
@@ -12,10 +10,16 @@ const appDir = path.resolve(argv.appdir || '.');
 const port = parseInt(argv.port) || 7001;
 const hostname = argv.hostname || '127.0.0.1';
 
-await plant.startApplication({
+const app = await plant.startApplication({
   appDir,
   hostname,
   port
 });
 
 console.log(`App(${appDir}) started at port ${port}`);
+
+process.on('SIGINT', async () => {
+  console.log('Received SIGINT signal, shutting down...');
+  await app.close();
+  process.exit(0);
+});
